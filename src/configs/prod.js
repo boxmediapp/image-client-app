d86111b3837a2175a4e5f8bc463c6eb2c1a2109a
Dilshat Hewzulla <hewzulla@gmail.com>
2017-12-09 22:24:07 +0000

    BIAD-71

--- src/configs/prod.js
+++ src/configs/prod.js
@@ -1,7 +1,7 @@
 var config={
     apikey:"k7jc3QcMPKEXGW5UC",
     securityGroup:"1CNbWCFpsbmRQuKdd",
-    version:"1.1.10",
+    version:"1.1.12",
     api:{
       base:"https://mediaapp.boxplus.com/mule/boxtv/",
       getUrl(path){
