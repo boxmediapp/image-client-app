c46050dbeede48d6d47c86d3c309b02f7fb847b7
Dilshat Hewzulla <hewzulla@gmail.com>
2018-11-15 12:49:42 +0000

    releasing

--- src/configs/local.js
+++
@@ -1,6 +0,0 @@
-import config from "./prod";
-config.api.base="http://localb/mule/boxtv/";
-config.url="https://image.boxnetwork.co.uk";
-config.apikey="k7jc3QcMPKEXGW5UC";
-config.securityGroup="1CNbWCFpsbmRQuKdd";
-export default config;
